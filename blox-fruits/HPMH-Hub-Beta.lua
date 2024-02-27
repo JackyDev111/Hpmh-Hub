d64ad1600e849209cd00a108b6c616fbe3ddc4de
HP IT | Tech <135197800+JackyDev111@users.noreply.github.com>
2024-02-27 16:45:39 +0700

    Update HPMH-Hub-Beta.lua

--- blox-fruits/HPMH-Hub-Beta.lua
+++ blox-fruits/HPMH-Hub-Beta.lua
@@ -1,3 +1,5 @@
-getgenv().Key = "MARU-ZEYQ-GJYZX-SULL-4H7LO-V8FY"
-getgenv().id = "920554890030379069"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+-- If you have more than 1 key, select only 1.
+_G.Key = "LuxuryV2_p506ahreinpx2cx4l5gn"
+
+_G.DiscordId = "920554890030379069"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NightsTimeZ/RoyryX/main/mainpack.lua"))();
