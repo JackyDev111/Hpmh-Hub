b87037692ed548caa30b9a5d499e4cf61bfb42f9
HP IT | Tech <135197800+JackyDev111@users.noreply.github.com>
2024-03-29 20:27:21 +0700

    Update HPMH-Hub-Beta.lua

--- blox-fruits/HPMH-Hub-Beta.lua
+++ blox-fruits/HPMH-Hub-Beta.lua
@@ -1,98 +1,162 @@
-_G.KaitanMode = true
-shared.Team = "Pirates"
-getgenv().Configs = {
-    FpsBoost = true,
-    SkipFarm = true,
-    HopIfCantKill = true,
-    BlockAllHop = false,
-
-    FastAttack = true,
-    NewFastAttack = true,
-    NoAttackAnimation = true,
-    
-    StartKaitun = true,
-    -- World 1
-    AutoPole = true, -- จะตีเเค่ถ้ามันเกิดไม่ได้ตีจนกว่าจะได้
-    AutoSaber = true,
-    
-    AutoSecondSea = true,
-    -- World 2
-    AutoRengoku = true,
-    AutoQuestFlower = true,
-    AutoRaceV3 = true,
-    AutoBartiloQuest = true,
-    AutoCursedCaptain = true,
-    AutoDarkbeard = true,
-    AutoFactory = true,
-    AutoThirdSea = true,
-    SkipGetItemGuitar = true, -- จะไม่ หาของทำ soul guiter ในโลก 2 เบบ หาจนกว่าจะได้ will not find item until get all item for do soul guiter ( open recommend เเนะนำให้เปิด )
-    AlliesFruit = {"Dragon-Dragon","Spirit-Spirit","Venom-Venom","Dough-Dough"}, -- จะไม่ใช้ผลพวกนี้ในการเปิดประตูไปโลก3
-    -- World 3
-    AutoHallowScythe = true,
-    AutoBuddySword = true,
-    AutoDoughKing = true,
-    AutoSpikeyTrident = true,
-    AutoTushita = true,
-    AutoEliteHunter = true,
-    AutoDarkDagger = true,
-    AutoYama = true,
-    AutoCanvander = true,
-    AutoSoulGuitar = true, 
-    AutoRainbowHaki = true,
-    AutoCursedDualKatana = true,
-    
-    -- Fighting Style 
-    
-    AutoGodHuman = true,
-    AutoSuperhuman = true,
-    AutoDeathStep = true,
-    AutoSharkmanKarate = true,
-    AutoElectricClaw = true,
-    AutoDargonTalon = true,
-    
-    AutoDFMastery = true,
-    SettingsSkill = { -- ถ้าไม่ใส่จะใช้ mode auto
-        -- ["Z"] = 0.1,
-        -- ["X"] = 0.1,
-        -- ["C"] = 0.1,
-        -- ["V"] = 0.1, -- อันไหนไม่เอาลบออกไปเลย
+-- Enabled BountyMode
+_G.BountyMode = true
+shared.Team = "Pirates" -- "Marines" & "Pirates"
+_G.BountyConfig = {
+    ["Webhook"] = {
+        ["Enabled"] = false,
+        ["Url"] = "",
+        ["CustomImage"] = "", -- if not in put = auto img
+        ["Mode Send"] = {"On Bounty Change","On Server Hop"}
     },
-    AutoSwordMastery = true,
-    SelectRaritySword = {"Mythical","Legendary"}, -- Common , Uncommon,Rare,Legendary,Mythical
-    
-    SelectRedeemCodeLevel = 1,
-    
-    -- Raids
-    
-    SelectRateFruitRaid = 1000000, -- if fruit price less u rate then we use it to auto raids
-    LimitFragmentsRaids = 100000,
-    
-    -- Devil Fruit
-        
-    SelectMainDF = {"Dragon-Dragon","Spirit-Spirit","Venom-Venom","Dough-Dough"}, -- ผลหลักที่จะกินเเทนผลรอง
-    SelectSubDF = {"Ice-Ice","Sand-Sand","Dark-Dark","Quake-Quake","Light-Light"}, -- ผลรองจะกินไว้ก่อนเเล้วพอผลหลักมีก้จะเปลียนไปกินผิดหลัก
-    AllowEatDFInventory = true,
-    StartSniper = true,
-        
-    -- RAM
-    
-    RAMPort = 7963,
-    RAMPassword = "",
-    AutoDescription = false,
-    
-    -- Webhook
-    
-    StartWebhook = true,
-    WebhookURL = "",
-    WebhookMode = "Send Every 10 min", -- "Send Every 10 min","Send On Level Max And Every 10 min"
-    
-    -- CPU
-    
-    LockFPS = 120,
-    LockFPSNow = true,
-    WhiteScreen = false
+    ["Fps Lock"] = {
+        ["Enabled"] = false, -- not recommend
+        ["Fps"] = 30
+    },
+    ["Lock Bounty"] = {
+        ["Enabled"] = false,
+        ["Cap"] = 30 -- ( mean 30m )
+    },
+    ["Add On"] = {
+        ["Skip Fuck User"] = true, -- skip door and budha 
+        ["Server Hop Region"] = "", -- find with u self '=' can use abbreviation like jap , tok if not put = auto
+        ["Add Range"] = 0, -- add range from u and target
+        ["Black Screen"] = false,
+        ["Hop Server Time"] = {
+            ["Enabled"] = true,
+            ["Delay Hop Time"] = 170, -- 2 min 30 sec -- and normal is hop when kill all player in server
+        } ,
+        ["Chat Sender"] = {
+            ["Enabled"] = false ,
+            ["Text"] = {"Go Buy Luxury Now","Luxury da bezt","im stronger than u",'i just too good'},
+        },
+
+        ["% Health To Save"] = 35, -- this is if u health lower 35% it will be go safe zone
+        ["% Health To Kill"] = 80, -- this is if u health higher it will kill 
+        ["Reset Character Save Zone"] = true, -- reset chearacter from save zone if not in combat
+
+        ["On Race V4"] = {
+            ["Enabled"] = true ,
+            ["Use This Skill on v4"] = { -- add skill that off/not have in custom method to this 
+                ["Gun"] = {
+                    ["Enabled"] = true,
+                    ["Delay"] = 0,
+                    ["Skills"] = {
+                        ["Z"] = {
+                            ["Enable"] = true,
+                            ["HoldTime"] = 0,
+                            ["Go Near Player"] = false,
+                        },
+                        ["X"] = {
+                            ["Enable"] = true,
+                            ["HoldTime"] = 0,
+                            ["Go Near Player"] = false,
+                        },
+                    },
+                },
+            }
+        },
+
+        ["IgnoreFriends"] = true, --Not Kill U Friends
+        ["Fast Mode"] = false, --Fast Mode
+    },
+    ["Skill"] = { -- just select one in this
+        ["Gun Method"] = false,
+
+        ["Auto Mode Beta"] = true, -- beta
+
+        ["Custom Method"] = {
+            ["Enabled"] = true,
+            ["Melee"] = {
+                ["Enabled"] = true,
+                ["Delay"] = 0.2,
+                ["Skills"] = {
+                    ["Z"] = {
+                        ["Enable"] = true,
+                        ["HoldTime"] = 2,
+                        ["Go Near Player"] = true,
+                    },
+                    ["X"] = {
+                        ["Enable"] = true,
+                        ["HoldTime"] = 0,
+                        ["Go Near Player"] = true,
+                    },
+
+                    ["C"] = {
+                        ["Enable"] = true,
+                        ["HoldTime"] = 0.1,
+                        ["Go Near Player"] = true,
+                    },
+                },
+            },
+            ["Blox Fruit"] = {
+                ["Enabled"] = false,
+                ["Delay"] = 0.3,
+                ["Skills"] = {
+                    ["Z"] = {
+                        ["Enable"] = true,
+                        ["HoldTime"] = 1,
+                        ["Go Near Player"] = true,
+                    },
+                    ["X"] = {
+                        ["Enable"] = true,
+                        ["HoldTime"] = 0,
+                        ["Go Near Player"] = true,
+                    },
+                    ["C"] = {
+                        ["Enable"] = false,
+                        ["HoldTime"] = 0,
+                        ["Go Near Player"] = true,
+                    },
+                    ["V"] = {
+                        ["Enable"] = true,
+                        ["HoldTime"] = 0.2,
+                        ["Go Near Player"] = true,
+                    },
+                    ["F"] = {
+                        ["Enable"] = true,
+                        ["HoldTime"] = 0,
+                        ["Go Near Player"] = true,
+                    },
+                },
+            },
+            -- ["Gun"] = { -- thin this example i add this in to on v4 use
+            --     ["Enabled"] = false,
+            --     ["Delay"] = 0,
+            --     ["Skills"] = {
+            --         ["Z"] = {
+            --             ["Enable"] = true,
+            --             ["HoldTime"] = 1,
+            --             ["Go Near Player"] = false,
+            --         },
+            --         ["X"] = {
+            --             ["Enable"] = true,
+            --             ["HoldTime"] = 0,
+            --             ["Go Near Player"] = false,
+            --         },
+            --     },
+            -- },
+            ["Sword"] = {
+                ["Enabled"] = true,
+                ["Delay"] = 0.2,
+                ["Skills"] = {
+                    ["Z"] = {
+                        ["Enable"] = true,
+                        ["HoldTime"] = 0,
+                        ["Go Near Player"] = true,
+                    },
+                    ["X"] = {
+                        ["Enable"] = true,
+                        ["HoldTime"] = 0,
+                        ["Go Near Player"] = false,
+                    },
+                },
+            },
+        }
+    }
 }
 
+-- If you have more than 1 key, select only 1.
 _G.Key = "LuxuryV2_p506ahreinpx2cx4l5gn"
+
 _G.DiscordId = "920554890030379069"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NightsTimeZ/RoyryX/main/mainpack.lua"))();
