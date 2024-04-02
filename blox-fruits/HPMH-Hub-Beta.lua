5f0a31dafa909ea75eb3f90a283e378ae5a3ab76
HP IT | Tech <135197800+JackyDev111@users.noreply.github.com>
2024-04-02 22:50:21 +0700

    Update HPMH-Hub-Beta.lua

--- blox-fruits/HPMH-Hub-Beta.lua
+++ blox-fruits/HPMH-Hub-Beta.lua
@@ -1,162 +1,109 @@
-shared.Team = "Pirates" -- "Marines" & "Pirates"
-_G.BountyConfig = {
-    ["Webhook"] = {
-        ["Enabled"] = false,
-        ["Url"] = "",
-        ["CustomImage"] = "", -- if not in put = auto img
-        ["Mode Send"] = {"On Bounty Change","On Server Hop"}
-    },
-    ["Fps Lock"] = {
-        ["Enabled"] = false, -- not recommend
-        ["Fps"] = 30
-    },
-    ["Lock Bounty"] = {
-        ["Enabled"] = false,
-        ["Cap"] = 30 -- ( mean 30m )
-    },
-    ["Add On"] = {
-        ["Skip Fuck User"] = true, -- skip door and budha 
-        ["Server Hop Region"] = "", -- find with u self '=' can use abbreviation like jap , tok if not put = auto
-        ["Add Range"] = 0, -- add range from u and target
-        ["Black Screen"] = false,
-        ["Hop Server Time"] = {
-            ["Enabled"] = true,
-            ["Delay Hop Time"] = 170, -- 2 min 30 sec -- and normal is hop when kill all player in server
-        } ,
-        ["Chat Sender"] = {
-            ["Enabled"] = false ,
-            ["Text"] = {"Go Buy Luxury Now","Luxury da bezt","im stronger than u",'i just too good'},
-        },
-
-        ["% Health To Save"] = 35, -- this is if u health lower 35% it will be go safe zone
-        ["% Health To Kill"] = 80, -- this is if u health higher it will kill 
-        ["Reset Character Save Zone"] = true, -- reset chearacter from save zone if not in combat
-
-        ["On Race V4"] = {
-            ["Enabled"] = true ,
-            ["Use This Skill on v4"] = { -- add skill that off/not have in custom method to this 
-                ["Gun"] = {
-                    ["Enabled"] = true,
-                    ["Delay"] = 0,
-                    ["Skills"] = {
-                        ["Z"] = {
-                            ["Enable"] = true,
-                            ["HoldTime"] = 0,
-                            ["Go Near Player"] = false,
-                        },
-                        ["X"] = {
-                            ["Enable"] = true,
-                            ["HoldTime"] = 0,
-                            ["Go Near Player"] = false,
-                        },
-                    },
-                },
-            }
-        },
-
-        ["IgnoreFriends"] = true, --Not Kill U Friends
-        ["Fast Mode"] = false, --Fast Mode
-    },
-    ["Skill"] = { -- just select one in this
-        ["Gun Method"] = false,
-
-        ["Auto Mode Beta"] = true, -- beta
-
-        ["Custom Method"] = {
-            ["Enabled"] = true,
-            ["Melee"] = {
-                ["Enabled"] = true,
-                ["Delay"] = 0.2,
-                ["Skills"] = {
-                    ["Z"] = {
-                        ["Enable"] = true,
-                        ["HoldTime"] = 2,
-                        ["Go Near Player"] = true,
-                    },
-                    ["X"] = {
-                        ["Enable"] = true,
-                        ["HoldTime"] = 0,
-                        ["Go Near Player"] = true,
-                    },
-
-                    ["C"] = {
-                        ["Enable"] = true,
-                        ["HoldTime"] = 0.1,
-                        ["Go Near Player"] = true,
-                    },
-                },
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players
+repeat task.wait() until game.Players.LocalPlayer
+repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+repeat task.wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
+_G.Team = "Pirate" -- Marine / Pirate
+_G.KAITUN_SCRIPT = true
+_G.LogsDes = {
+    ["Enabled"] = true, -- เปิดการใช้งาน
+    ["SendAlias"] = true, -- เปิดการส่ง Alias
+    ["SendDescription"] = true, -- เปิดการส่ง Des
+    ["DelaySend"] = 5 -- วินาที
+}
+_G.WebHook = {
+    ["Enabled"] = true, -- เปิดการใช้งาน
+    ["Url"] = "", -- ลิ้งค์เว็บฮุก
+    ["Delay"] = 60 -- วินาที
+}
+_G.MainSettings = {
+        ["EnabledHOP"] = true, -- เปิด HOP ( มันไม่มีอยู่ละใส่มาเท่ๆ )
+        ['FPSBOOST'] = true, -- ภาพกาก
+        ["FPSLOCKAMOUNT"] = 30, -- จำนวน FPS
+        ['WhiteScreen'] = false, -- จอขาว
+        ['CloseUI'] = false, -- ปิด Ui
+        ["NotifycationExPRemove"] = true, -- ลบ ExP ที่เด้งตอนฆ่ามอน
+        ['AFKCheck'] = 150, -- ถ้ายืนนิ่งเกินวิที่ตั้งมันจะรีเกม
+        ["LockFragments"] = 20000, -- ล็อคเงินม่วง
+        ["LockFruitsRaid"] = { -- ล็อคผลที่ไม่เอาไปลงดัน
+            [1] = "Dough-Dough",
+            [2] = "Dragon-Dragon"
+        }
+    }
+_G.Fruits_Settings = { -- ตั้งค่าผล
+    ['Main_Fruits'] = {'Dough-Dough'}, -- ผลหลัก ถ้ายังไม่ใช่ค่าที่ตั้งมันจะกินจนกว่าจะใช่หรือซื้อ
+    ['Select_Fruits'] = {"Flame-Flame", "Ice-Ice", "Quake-Quake", "Light-Light", "Dark-Dark", "Spider-Spider", "Rumble-Rumble", "Magma-Magma", "Buddha-Buddha"} -- กินหรือซื้อตอนไม่มีผล
+}
+_G.Quests_Settings = { -- ตั้งค่าเควสหลักๆ
+    ['Rainbow_Haki'] = true,
+    ["MusketeerHat"] = true,
+    ["PullLever"] = true,
+    ['DoughQuests_Mirror'] = {
+        ['Enabled'] = true,
+        ['UseFruits'] = true
+    }        
+}
+_G.Races_Settings = { -- ตั้งค่าเผ่า
+    ['Race'] = {
+        ['EnabledEvo'] = true,
+        ["v2"] = true,
+        ["v3"] = true,
+        ["Races_Lock"] = {
+            ["Races"] = { -- Select Races U want
+                ["Mink"] = true,
+                ["Human"] = true,
+                ["Fishman"] = true
             },
-            ["Blox Fruit"] = {
-                ["Enabled"] = false,
-                ["Delay"] = 0.3,
-                ["Skills"] = {
-                    ["Z"] = {
-                        ["Enable"] = true,
-                        ["HoldTime"] = 1,
-                        ["Go Near Player"] = true,
-                    },
-                    ["X"] = {
-                        ["Enable"] = true,
-                        ["HoldTime"] = 0,
-                        ["Go Near Player"] = true,
-                    },
-                    ["C"] = {
-                        ["Enable"] = false,
-                        ["HoldTime"] = 0,
-                        ["Go Near Player"] = true,
-                    },
-                    ["V"] = {
-                        ["Enable"] = true,
-                        ["HoldTime"] = 0.2,
-                        ["Go Near Player"] = true,
-                    },
-                    ["F"] = {
-                        ["Enable"] = true,
-                        ["HoldTime"] = 0,
-                        ["Go Near Player"] = true,
-                    },
-                },
-            },
-            -- ["Gun"] = { -- thin this example i add this in to on v4 use
-            --     ["Enabled"] = false,
-            --     ["Delay"] = 0,
-            --     ["Skills"] = {
-            --         ["Z"] = {
-            --             ["Enable"] = true,
-            --             ["HoldTime"] = 1,
-            --             ["Go Near Player"] = false,
-            --         },
-            --         ["X"] = {
-            --             ["Enable"] = true,
-            --             ["HoldTime"] = 0,
-            --             ["Go Near Player"] = false,
-            --         },
-            --     },
-            -- },
-            ["Sword"] = {
-                ["Enabled"] = true,
-                ["Delay"] = 0.2,
-                ["Skills"] = {
-                    ["Z"] = {
-                        ["Enable"] = true,
-                        ["HoldTime"] = 0,
-                        ["Go Near Player"] = true,
-                    },
-                    ["X"] = {
-                        ["Enable"] = true,
-                        ["HoldTime"] = 0,
-                        ["Go Near Player"] = false,
-                    },
-                },
-            },
+            ["RerollsWhenFragments"] = 20000 -- Random Races When Your Fragments is >= Settings
         }
     }
 }
+_G.Settings_Melee = { -- หมัดที่จะทำ
+    ['Superhuman'] = true,
+    ['DeathStep'] = true,
+    ['SharkmanKarate'] = true,
+    ['ElectricClaw'] = true,
+    ['DragonTalon'] = true,
+    ['Godhuman'] = true
+}
+_G.FarmMastery_Settings = {
+    ['Melee'] = true,
+    ['Sword'] = true,
+    ['DevilFruits'] = true,
+    ['Select_Swords'] = {
+        ["AutoSettings"] = true, -- ถ้าเปิดอันนี้มันจะเลือกดาบให้เองหรือฟาร์มทุกดาบนั่นเอง
+        ["ManualSettings"] = { -- ถ้าปรับ AutoSettings เป็น false มันจะฟาร์มดาบที่เลือกตรงนี้ ตัวอย่างข้างล่าง
+            "Saber",
+            "Buddy Sword"
+        }
+    }
+}
+_G.SwordSettings = { -- ดาบที่จะทำ
+    ['Saber'] = true,
+    ["Pole"] = true,
+    ['MidnightBlade'] = true,
+    ['Shisui'] = true,
+    ['Saddi'] = true,
+    ['Wando'] = true,
+    ['Yama'] = true,
+    ['Rengoku'] = true,
+    ['Canvander'] = true,
+    ['BuddySword'] = true,
+    ['TwinHooks'] = true,
+    ['HallowScryte'] = true,
+    ['TrueTripleKatana'] = true,
+    ['CursedDualKatana'] = true
+}
+_G.SharkAnchor_Settings = {
+    ["Enabled_Farm"] = true,
+}
+_G.GunSettings = { -- ปืนที่จะทำ
+    ['Kabucha'] = true,
+    ['SerpentBow'] = true,
+    ['SoulGuitar'] = true
+}
 
--- Enabled BountyMode
-_G.BountyMode = true
--- If you have more than 1 key, select only 1.
-_G.Key = "LuxuryV2_p506ahreinpx2cx4l5gn"
-
-_G.DiscordId = "920554890030379069"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NightsTimeZ/RoyryX/main/mainpack.lua"))();
+getgenv().Key = "MARU-ZEYQ-GJYZX-SULL-4H7LO-V8FY"
+getgenv().id = "920554890030379069"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
