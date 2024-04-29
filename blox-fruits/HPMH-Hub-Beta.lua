9cbc9422059c0ddb62c01338fff5331b2b98dc13
HP IT | Tech <135197800+JackyDev111@users.noreply.github.com>
2024-04-30 06:32:00 +0700

    Update HPMH-Hub-Beta.lua

--- blox-fruits/HPMH-Hub-Beta.lua
+++ blox-fruits/HPMH-Hub-Beta.lua
@@ -1,4 +1,3 @@
 getgenv().Key = "MARU-ZEYQ-GJYZX-SULL-4H7LO-V8FY"
 getgenv().id = "920554890030379069"
-getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
