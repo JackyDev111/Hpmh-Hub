e91fe6ff1ee8d60f29580a426b67778a58fe9a55
HP IT | Tech <135197800+JackyDev111@users.noreply.github.com>
2024-05-04 06:54:25 +0700

    Update HPMH-Hub-Beta.lua

--- blox-fruits/HPMH-Hub-Beta.lua
+++ blox-fruits/HPMH-Hub-Beta.lua
@@ -1,4 +1,10 @@
-getgenv().Key = "MARU-ZEYQ-GJYZX-SULL-4H7LO-V8FY"
-getgenv().id = "920554890030379069"
-getgenv().Script_Mode = "Kaitun_Script"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+-- getgenv().Key = "MARU-ZEYQ-GJYZX-SULL-4H7LO-V8FY"
+-- getgenv().id = "920554890030379069"
+-- getgenv().Script_Mode = "Kaitun_Script"
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+
+getgenv().Team = "Marines" -- Pirates/Marines
+getgenv().Fix_Lag = true -- true/false
+getgenv().Auto_Execute = false -- true/false
+getgenv().Clear_Settings = false -- true/false
+loadstring(game:HttpGet("https://xerohub.click/script/main.lua"))() 
