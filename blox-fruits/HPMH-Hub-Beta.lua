e1013e0e91a21112b1491721d3068b45ba1d53d5
HP IT | Tech <135197800+JackyDev111@users.noreply.github.com>
2024-05-05 15:23:58 +0700

    Update HPMH-Hub-Beta.lua

--- blox-fruits/HPMH-Hub-Beta.lua
+++ blox-fruits/HPMH-Hub-Beta.lua
@@ -1,4 +1,111 @@
-getgenv().Key = "MARU-ZEYQ-GJYZX-SULL-4H7LO-V8FY"
-getgenv().id = "920554890030379069"
-getgenv().Script_Mode = "Kaitun_Script"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+getgenv().Configs = {
+    ["Team"] = "Pirates", -- Pirates/Marines
+    ["Auto Ken"] = true,
+    ["Auto Buso"] = true,
+    ["Auto turn on race v3"] = true,
+    ["Auto turn on race v4"] = false,
+    ["Auto summon sea beast"] = true,
+    ["FPS Boost"] = false,
+    ["Click Time"] = 0.2,
+    ["Bypass TP"] = true,
+    ["Chat"] = {
+        ["Enable"] = false,
+        ["Message"] = {"get good losers", "no skill?", "dont cry xd", "u cant win lol", "try to hunt me", "get a good gaming chair losers", "feeling no skill?"}, -- Input your message here
+    },
+    ["Run When Low Health"] = {
+        ["Enable"] = true,
+        ["Health"] = 3500,
+        ["Come back"] = false
+    },
+    ["Region"] = "Singapore", -- Singapore, United States, Germany, France, India, ??? ...
+    ["Weapons"] = {
+        ["Melee"] = {
+            ["Enable"] = true,
+            ["Delay"] = 1.75,
+            ["Skills"] = {
+                ["Z"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 1,
+                },
+                ["X"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0,
+                },
+                ["C"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0,
+                }
+            }
+        },
+        ["Fruit"] = {
+            ["Enable"] = false,
+            ["Delay"] = 1.8,
+            ["Skills"] = {
+                ["Z"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0,
+                },
+                ["X"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0,
+                },
+
+                ["C"] = {
+                    ["Enable"] = false,
+                    ["HoldTime"] = 0,
+                },
+                ["V"] = {
+                    ["Enable"] = false,
+                    ["HoldTime"] = 0,
+                },
+                ["F"] = {
+                    ["Enable"] = false,
+                    ["HoldTime"] = 0,
+                }
+            }
+        },
+        ["Sword"] = {
+            ["Enable"] = true,
+            ["Delay"] = 1,
+            ["Skills"] = {
+                ["Z"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0,
+                },
+                ["X"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0,
+                }
+            }
+        },
+        ["Gun"] = {
+            ["Enable"] = false,
+            ["Delay"] = 0.5,
+            ["Skills"] = {
+                ["Z"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0,
+                },
+                ["X"] = {
+                    ["Enable"] = true,
+                    ["HoldTime"] = 0,
+                }
+            }
+        }
+    },
+    ["Theme"] = {
+        ["Enable"] = false,
+        ["Name"] = "Hutao", -- Hutao, Raiden, Ayaka, Yelan
+        ["Custom Theme"] = {
+            ["Enable"] = false,
+            ["Text Color"] = Color3.fromRGB(231, 85, 88),
+            ["Character Position"] = UDim2.new(0.563000023, 0, -0.174999997, 0)
+        }
+    },
+    ["Webhook"] = {
+        ["Enable"] = false,
+        ["Url"] = "",
+        ["Image"] = ""
+    }
+}
+loadstring(game:HttpGet("https://xerohub.click/script/autobounty.lua"))() 
